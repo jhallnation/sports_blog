0616573ff143ac25fbb1da118cd800e84614cd04
Jason Hall <jhallnation@gmail.com>
2017-11-01 12:30:28 -0600

    added toggle to new blog form

--- src/Components/NewBlog.js
+++ src/Components/NewBlog.js
@@ -4,8 +4,10 @@ export class NewBlog extends Component {
   constructor(){
     super();
     this.state = { 
-      newBlog: {}
+      newBlog: {},
+      display: '' 
     }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
   }
   handleSubmit(e){
     if(this.refs.title.value === ''){
@@ -20,11 +22,21 @@ export class NewBlog extends Component {
     }
     e.preventDefault();
   }
+
+  componentWillMount(){
+    this.setState({ display: 'none' });
+  }
+
+   toggleDisplay() {
+    const newDisplay = this.state.display === 'none' ? 'block' : 'none';
+    this.setState({ display: newDisplay });
+  }
+  
   render(){
     return ( 
       <div>
-        <h4>New Blog</h4>
-        <form onSubmit={this.handleSubmit.bind(this)}>
+        <h4 onClick={this.toggleDisplay } >New Blog</h4>
+        <form onSubmit={this.handleSubmit.bind(this)} style={{display:this.state.display}} >
           <div>
             <label>Title</label><br />
             <input type='text' ref='title' />
